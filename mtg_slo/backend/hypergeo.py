a686a259bccbc5ac8494e297b337db417a19b069
TNT670 <tniangtrost@gmail.com>
2020-03-16 18:15:28 -0500

    Added heuristic function in backend

--- mtg_slo/backend/hypergeo.py
+++ mtg_slo/backend/hypergeo.py
@@ -1,11 +1,30 @@
 
 
 # from scipy.special import binom
-from math import factorial
+from math import ceil, factorial
 
 # n! / [k!(n-k)!] for (n choose k)
 def binom(n,k):
 	return factorial(n) / ( factorial(k) * factorial(n-k) )
+
+def LsHeuristic(manaCosts: list):
+	""" Returns a list representing the heuristic to be used. Rounds up; the
+	    returned list should add up to either 17 or 18.
+	
+	Keyword Arguments:
+		manaCosts: list of mana costs of the form (generic, (W,U,B,R,G)).
+	
+	Example:
+		manaCosts = [(2, (0,0,0,1,1)), (2, (0,0,0,1,0)), (2, (0,0,0,0,1)),
+		             (1, (0,0,0,1,0)), (0, (0,0,0,1,1)), (1, (0,0,0,0,1)),
+					 (0, (0,0,0,1,0)), (0, (0,0,0,0,1))]
+		res = [0, 0, 0, 9, 9]
+	"""
+    lands = [0,0,0,0,0]
+	for elem in manaCosts:
+	    lands = [lands[i] + elem[1][i] for i in range(0,5)]
+	res = [ceil(x * 17 / sum(lands)) for x in lands]
+	return res
 
 def MVHG(pop,req,totalPop):
 	""" Returns probability
@@ -32,15 +51,16 @@ def MVHG(pop,req,totalPop):
 	return num / den
 
 
-def cardCastibility(landSet:tuple, manaCost: str):
+def cardCastibility(landSet:list, manaCost:tuple):
 	""" Returns odds of being able to cast a card on curve
 
 	Keyword Arguments:
 		landSet: tuple of lands. Of form (W,U,B,R,G), with 0 for no lands
 				(0,0,0,10,7) would represent a set of lands of 10 Mountain 7 Forest
 		
-		manaCost: string of mana cost, of form '2RG'
+		manaCost: tuple of mana cost (generic, (W,U,B,R,G))
 	"""
+	
 	# Each of these casts. Programmatically find set
 	# (3,1) rrrg
 	# (2,2) rrgg
